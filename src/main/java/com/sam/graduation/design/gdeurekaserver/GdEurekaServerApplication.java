e2217aa8386bdd289e43304266b4d506fae17c81
sam199510 <273045049@qq.com>
2018-01-29 12:26:46 +0800

    毕业设计Eureka Server监听服务的第一次提交

---
+++ src/main/java/com/sam/graduation/design/gdeurekaserver/GdEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.sam.graduation.design.gdeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class GdEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdEurekaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sam/graduation/design/gdeurekaserver/GdEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sam.graduation.design.gdeurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GdEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
